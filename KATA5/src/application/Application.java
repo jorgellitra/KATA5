d25cd7c0535f371ddf4241b6ea4b18c4cbbfb0b4
usuario <usuario@10.22.143.7>
2015-11-11 10:09:17 +0000

    para sqlite

--- KATA5/src/application/Application.java
+++ KATA5/src/application/Application.java
@@ -1,5 +1,10 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -8,14 +13,21 @@ import java.sql.Statement;
 
 public class Application {
 
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException, FileNotFoundException, IOException {
         Class.forName("org.sqlite.JDBC");
-        Connection conec = DriverManager.getConnection("jdbc:sqlite:BDMails");
+        Connection conec = DriverManager.getConnection("jdbc:sqlite:BDMails.db");
         Statement state = conec.createStatement();
         ResultSet rs = state.executeQuery("SELECT * FROM PEOPLE");
-        System.out.println("ID = " + rs.getInt("ID"));
-        System.out.println("NAME = " + rs.getString("NAME"));
-                
+        String nameFile = "";
+        BufferedReader reader = new BufferedReader(new FileReader ( new File ( nameFile)));
+        String mail;
+        while((mail = reader.readLine())!= null){
+            String query = "INSERT INTO MAIL (MAIL) VALUES ('" + mail + "')";
+            state.executeQuery(query);
+        }
+        rs.close();
+        state.close();
+        conec.close();
     }
     
 }
